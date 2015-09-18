aa04e4ede5de555f7a3b65b199093bd1b3409f55
mirajp1 <mirajp1@gmail.com>
2015-09-18 12:59:35 +0530

    add article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917155150) do
+ActiveRecord::Schema.define(version: 20150918072343) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "articles", ["author_id"], name: "index_articles_on_author_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
